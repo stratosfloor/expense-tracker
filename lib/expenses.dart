26b6f73a0f25a819280c9daf6a298e93e7b129b0
Emil Noren <stratosfloor@hotmail.com>
2023-06-28 10:47:10 +0200

    Added expenses_list widget with temp hardcoded list

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
   @override
@@ -9,13 +12,33 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Beer',
+        amount: 99999,
+        date: DateTime.now(),
+        category: Category.leisure),
+    Expense(
+        title: 'Cinema',
+        amount: 1337,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('The Chart'),
-          Text('Expenses list'),
+          const Text('The Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          )
         ],
       ),
     );
